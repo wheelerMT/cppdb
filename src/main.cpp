b38845148925be6215c93a5a5c713febde3a1461
Matt Wheeler <wheelermt@proton.me>
2026-03-25 20:35:43 +0000

    Update main to avoid potential exception in std::print

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
+#include <ostream>
 #include <print>
 
 int main() {
-  std::print("cppdb v0.1.0 starting...");
+    try {
+        std::print("cppdb v0.1.0 starting...");
+    } catch (std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return 1;
+    } catch (...) {
+        std::cerr << "unknown exception" << '\n';
+        return 1;
+    }
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
