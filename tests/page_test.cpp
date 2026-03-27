f93ca47234eef0b406ce58a5a15726157c9e25ee
Matt Wheeler <wheelermt@proton.me>
2026-03-27 20:50:00 +0000

    test: add test for writing/read to a non-zero offset

--- tests/page_test.cpp
+++ tests/page_test.cpp
@@ -34,4 +34,18 @@ TEST_CASE("Page can be written to and read from", "[page]") {
 
         REQUIRE(std::ranges::equal(readData, writeData));
     }
+
+    SECTION("data can be correctly written to a non-zero offset") {
+        constexpr std::array initialData{std::byte{1}, std::byte{2}, std::byte{3}, std::byte{4}};
+        constexpr std::array offsetData{std::byte{10}, std::byte{20}, std::byte{30}, std::byte{40}};
+
+        page.write(0, initialData);
+        page.write(8, offsetData);
+
+        const auto initialReadData = page.read(0, 4);
+        const auto offsetReadData = page.read(8, 4);
+
+        REQUIRE(std::ranges::equal(initialData, initialReadData));
+        REQUIRE(std::ranges::equal(offsetData, offsetReadData));
+    }
 }
